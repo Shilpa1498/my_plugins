d2e16165d164175ee130c8a8075340be4e896d53
shilpamadhu <shilpa@thetopclassentertainment.com>
2023-04-20 16:20:29 +0530

    new files added

--- lib/dreamlive_plugins.dart
+++
@@ -1,7 +0,0 @@
-library dreamlive_plugins;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
---
+++ plugins/lib/dreamlive_plugins.dart
@@ -0,0 +1,12 @@
+library dreamlive_plugins;
+
+export 'avatar_glow.dart';
+export 'custom_pop_up_menu.dart';
+export 'dotted_border.dart';
+export 'flutter_switch.dart';
+export 'gif.dart';
+export 'gradient_borders.dart';
+export 'marquee.dart';
+export 'outline_gradient_button.dart';
+export 'stop_watch_timer.dart';
+export 'mime_type.dart';
